b88fe9f3390d72b31ca8d2e5c3a33cd806a5d03d
shinyscyther <mesibovj@gmail.com>
2019-01-24 14:08:12 -0500

    added code for capturing command line arguments to search spotifys node api

--- liri.js
+++ liri.js
@@ -1,7 +1,15 @@
 require("dotenv").config();
+var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new spotify(keys.spotify);
 
-  if (process.argv[1] === "spotify-this-song") {
-
+if (process.argv[2] === "spotify-this-song") {
+  spotify
+    .search({ type: 'track', query: process.argv[3] })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
   }
