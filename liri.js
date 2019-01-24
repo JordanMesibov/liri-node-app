5ed83391ab4bbf547bda1ee715501f7657ea6c5a
shinyscyther <mesibovj@gmail.com>
2019-01-24 14:16:32 -0500

    changed the specific response that is being console logged following the spotify API call in order to get data that I can work with

--- liri.js
+++ liri.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-var spotify = new spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 if (process.argv[2] === "spotify-this-song") {
   spotify
-    .search({ type: 'track', query: process.argv[3] })
+    .search({ type: 'track', query: process.argv[3], limit: 1 })
     .then(function(response) {
-      console.log(response);
+      console.log(response.tracks.items);
     })
     .catch(function(err) {
       console.log(err);
